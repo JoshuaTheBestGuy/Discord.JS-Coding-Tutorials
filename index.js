c512112360c8c5e7461ab8431ba0efdd35c6c1b7
joshua <jhendrickson2006@gmail.com>
2020-06-21 13:09:58 -0700

    some stuff

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ fs.readdir("./commands/", (err, files) => {
 
     if(err) console.log(err)
 
-    let jsfile = files.filter(f => f.split(".").pop() === "js") 
+    let jsfile = files.filter(f => f.split(".").pop() === "js")
     if(jsfile.length <= 0) {
          return console.log("[LOGS] Couldn't Find Commands!");
     }
 
     jsfile.forEach((f, i) => {
         let pull = require(`./commands/${f}`);
-        bot.commands.set(pull.config.name, pull);  
+        bot.commands.set(pull.config.name, pull);
         pull.config.aliases.forEach(alias => {
             bot.aliases.set(alias, pull.config.name)
         });
@@ -46,4 +46,4 @@ bot.on("message", async message => {
 
 })
 
-bot.login(botsettings.token);
+bot.login(process.env.token)
